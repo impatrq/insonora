23c31af8799a4cceb02ee111f021676d1578621b
francocamdessus <80331997+francocamdessus@users.noreply.github.com>
2023-11-21 08:48:23 -0300

    feat : subir codigo para generar senoidales desfasadas ESP32 CWG

---
+++ generadordesenoidalesdesfasadas.c
@@ -0,0 +1,15 @@
+// codigo usado para generar en un canal una senoidal y en el otro la misma pero 180 grados desfasadas
+#include "DacESP32.h"
+
+DacESP32 dac1(DAC_CHANNEL_1),
+         dac2(DAC_CHANNEL_2);
+
+void setup() {
+  dac1.outputCW(400);                // 2kHz sinus signal on pin 25
+  dac2.outputCW(400);   // steady voltage level of 1.3V on pin 26
+  //dac2.outputVoltage((uint8_t)100); // same as above but with 8-bit argument, range 0...255
+                                      // (Vout = 100/255 * 3.3V = ~1.3V)
+}
+void loop(){
+  
+}
